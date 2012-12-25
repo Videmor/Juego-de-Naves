8f509879cded44f27d1d223313e5d43aeee0412c
Jorge Crisóstomo <videmor@gmail.com>
2012-12-24 22:43:33 -0500

    draw nave

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 var canvas = document.getElementById('game');
 var ctx = canvas.getContext('2d');
 
+//crear el objeto de la nave
+var nave = {
+	x: 100,
+	y: 100,
+	width: 50,
+	height: 50
+}
+
 var fondo;
 
 function loadMedia(){
@@ -15,8 +23,16 @@ function drawBackground(){
 	ctx.drawImage(fondo,0,0);
 }
 
+function drawNave(){
+	ctx.save();
+	ctx.fillStyle = 'white';
+	ctx.fillRect(nave.x, nave.y, nave.width, nave.height );
+	ctx.restore();
+}
+
 function frameloop(){
 	drawBackground();
+	drawNave();
 }
 
 loadMedia();
