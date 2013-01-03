ebcdd785f92b3fae7d9a3486a96c0383151a0746
Jorge Crisóstomo <videmor@gmail.com>
2013-01-03 03:17:16 -0500

    invasores

--- main.js
+++ main.js
@@ -9,10 +9,14 @@ var nave = {
 	height: 50
 }
 
+var juego = {
+	estado: 'iniciando'
+}
+
 var teclado = [];
 
 var disparos = [];
-
+var enemigos = [];
 var fondo;
 
 function loadMedia(){
@@ -20,6 +24,17 @@ function loadMedia(){
 	fondo.src = 'img/fondo.jpg';
 	fondo.onload = function(){
 		var intervalo = window.setInterval(frameloop, 1000/55);
+	}
+}
+
+function dibujarEnemigos(){
+	for(var i in enemigos){
+		var enemigo =  enemigos[i];
+		ctx.save();
+		if (enemigo.estado == 'vivo') { ctx.fillStyle = 'red'};
+		if (enemigo.estado == 'muerto') { ctx.fillStyle = 'black' };
+		ctx.fillRect(enemigo.x, enemigo.y, enemigo.width, enemigo.height);
+
 	}
 }
 
@@ -87,8 +102,43 @@ function fire(){
 	disparos.push({
 		x: nave.x + 20,
 		y: nave.y - 10,
-		width: 2,
+		width: 5,
 		height: 30
+	})
+}
+
+function actualizaEnemigos(){
+	if (juego.estado == 'iniciando') {
+		for (var i = 0; i < 10; i++) {
+			enemigos.push({
+				x: 10 + (i*50),
+				y: 10,
+				height: 40,
+				width: 40,
+				estado: 'vivo',
+				contador: 0
+			})
+		};
+		juego.estado = 'jugando';
+	};
+	for (var i in enemigos) {
+		var enemigo =  enemigos[i];
+		if(!enemigo) continue;
+		if(enemigo && enemigo.estado == "vivo"){
+			enemigo.contador++;
+			enemigo.x += Math.sin(enemigo.contador * Math.PI / 90) * 9;
+		}
+		if (enemigo && enemigo.estado == "hit") {
+			enemigo.contador++;
+			if (enemigo.contador >= 20) {
+				enemigo.estado = 'muerto';
+				enemigo.contador = 0;
+			};
+		};
+	};
+	enemigos =  enemigos.filter(function(enemigo){
+		if (enemigo && enemigo.estado != 'muerto') { return true};
+		return false;
 	})
 }
 
@@ -102,10 +152,46 @@ function drawDisparos(){
 	ctx.restore();
 }
 
+function hit(a, b){
+	var hit = false;
+	if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+		if(b.y + b.height >= a.y && b.y < a.y + a.height){
+			hit =  true;
+		}
+	};
+	if (b.x <= a.x  && b.x + b.width >= a.x + a.width) {
+		if(b.y <= a.y && b.y + b.height >= a.y + a.height){
+			hit =  true;
+		}
+	};
+	if (a.x <= b.x  && a.x + a.width >= b.x + b.width) {
+		if(a.y <= b.y && a.y + a.height >= b.y + b.height){
+			hit =  true;
+		}
+	};
+	return hit;
+}
+
+function verificarContacto(){
+	for (var i in disparos) {
+		var disparo = disparos[i];
+		for (var j in enemigos) {
+			var enemigo = enemigos[j];
+			if (hit(disparo,enemigo)) {
+				enemigo.estado = 'hit';
+				enemigo.contador = 0;
+			};
+		};
+	};
+}
+
 function frameloop(){
 	moveNave()
+	actualizaEnemigos();
 	moveDisparos();
 	drawBackground();
+	verificarContacto();
+	dibujarEnemigos();
 	drawDisparos();
 	drawNave();
 }
